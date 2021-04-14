414e7f987e1509bd142ee22ef20086dbe8c3c490
Calvin Jacobsen <Jaco1932@umn.edu>
2021-04-14 09:31:58 -0500

    finished main, login and signup handlebars... continuing work on routes to each and implimenting redirecting functionality

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-// Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const hbs = exphbs.create({});
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-// Sets up the Express App
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/home-route'));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
 });
