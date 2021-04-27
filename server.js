0764ddc8e8a6b4a5bf29d03ab07063127cfbcaaa
Calvin Jacobsen <Jaco1932@umn.edu>
2021-04-26 19:51:16 -0500

    added readme documentation

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT;
 
 const hbs = exphbs.create({});
 
@@ -35,5 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+  app.listen(process.env.PORT || 3001, () => console.log('Now listening on PORT ' + PORT));
 });
